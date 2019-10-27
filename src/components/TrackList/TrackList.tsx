94043dde15689499ebda3ca8bba023f632a0d052
kasrakhosravi <kasrakhosravi2@gmail.com>
2019-10-27 13:49:53 +0100

    improvements to Tracklist

--- src/components/TrackList/TrackList.tsx
+++ src/components/TrackList/TrackList.tsx
@@ -2,11 +2,13 @@
 // global dependencies
 import React, { Component } from 'react';
 import moment from 'moment';
+import { Table } from 'reactstrap';
 import 'moment-duration-format';
 
 // local dependencies
 import IAlbum from '../../interfaces/Album';
 import ITrack from '../../interfaces/Track';
+import './TrackList.scss';
 
 interface IProps {
     tracks: Array<ITrack>,
@@ -19,8 +21,6 @@ class AlbumList extends Component<IProps> {
 
         // initialization
         const { tracks, album } = this.props;
-
-        console.log('track props', this.props);
 
         // local components
         const tracksView = (tracks: Array<ITrack>, album: IAlbum) => (
@@ -49,10 +49,35 @@ class AlbumList extends Component<IProps> {
             </table>
         );
 
+        const newTracksView = (tracks: Array<ITrack>, album: IAlbum) => (
+            <Table dark responsive borderless striped className="TrackListTable">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Time</th>
+                    <th>Released</th>
+                </tr>
+                </thead>
+                <tbody>
+                {tracks.map(track => (
+                    <tr key={track.id}>
+                        <td>{track.track_position}</td>
+                        <td>{track.title}</td>
+                        <td>{track.artist.name}</td>
+                        <td>{moment.duration(track.duration, 'minutes').format('h:mm')}</td>
+                        <td>{moment(album.release_date).format('YYYY')}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
+        );
+
         return (
-            <div>
-                {tracksView(tracks, album)}
-            </div>
+            <React.Fragment>
+                {newTracksView(tracks, album)}
+            </React.Fragment>
         );
     }
 }
