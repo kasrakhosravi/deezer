589ee5b11a5286d61196694eed39ac873b0db814
kasrakhosravi <kasrakhosravi2@gmail.com>
2019-10-27 21:57:21 +0100

    doc for Constants

--- src/constants/ApiConstants.ts
+++ src/constants/ApiConstants.ts
@@ -1,6 +1,11 @@
 
+// search
 export const SEARCH_ARTIST_URL = `/api/search/artist?q=:keyword&offset=0&limit=5`;
+
+// artist
 export const ARTIST_URL = `/api/artist/:id`;
 export const ARTIST_ALBUMS_URL = `${ARTIST_URL}/albums?limit=4`;
+
+// albums
 export const ALBUM_URL = `/api/album/:id`;
 export const ALBUM_TRACKS_URL = `${ALBUM_URL}/tracks`;
